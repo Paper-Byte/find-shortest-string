53a02b59624084c091f2e47283755f956f6be225
Noah <noahhughesph@gmail.com>
2023-10-11 21:48:49 -0400

    done

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,21 +1,30 @@
 function findShortestString(arr) {
-  // type your code here
+  let short = arr[0];
+  for (const curr of arr) {
+    if (curr.length < short.length) {
+      short = curr;
+    }
+  }
+  return short;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log('=>', findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log('=>', findShortestString(['cat', 'hi', 'dog', 'an']));
 
-  console.log("");
+  console.log('');
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log(
+    '=>',
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion'])
+  );
 
   // BENCHMARK HERE
 }
